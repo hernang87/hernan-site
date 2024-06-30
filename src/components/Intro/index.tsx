510fd845a9f23f169590a02e7c7bbae1c74f9d82
hernang87 <hernang@mural.co>
2024-06-30 15:29:53 -0300

    Add more text

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -3,9 +3,9 @@ import { HighlightedText } from "../HighlightedText";
 export function Intro() {
   return (
     <section className="intro">
-      <p>I'm a software developer based in Buenos Aires, Argentina.</p>
-      <p>Lately I've specialized on frontend with full stack capabilities.</p>
+      <p>I'm a <HighlightedText text="Software Engineer" /> based in Buenos Aires, Argentina 🇦🇷.</p>
       <p>I'm focused on <HighlightedText text="product companies" />, I truly care about our <HighlightedText text="customers" />, ensuring <HighlightedText text="solutions meet their needs" /> and <HighlightedText text="exceed expectations"/>.</p>
+      <p>I've been working on <HighlightedText text="frontend projects" /> lately, specially ones with <HighlightedText text="complex user interactions" /> defining how these <HighlightedText text="should be develeped" /> and creating their <HighlightedText text="executiones plans"/>.</p>
     </section>
   );
 }
